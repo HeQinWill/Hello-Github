97558cbaddd377e41d3c486ce49ae132304811a8
HeQin_Vultr <757407462@qq.com>
2020-10-26 23:26:27 +0800

    autoUpload

--- Upload.py
+++
@@ -1,7 +0,0 @@
-import os
-import time
-
-localtime = time.strftime("%Y%m%dT%H%M", time.localtime())
-os.system('/usr/bin/git add /root/qinGitHub/Hello-Github')
-os.system(f'/usr/bin/git commit -m autoUpload_{localtime}')
-os.system('/usr/bin/git push')
