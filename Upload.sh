794a2be175fbfd81431a1a2f36004b4051ee5e34
HeQin_Vultr <757407462@qq.com>
2020-10-26 23:48:29 +0800

    autoUpload

--- Upload.sh
+++ Upload.sh
@@ -1,4 +1,9 @@
-#! /bin/bash
+#!/bin/bash
+echo "1"
+cd /root/qinGitHub/Hello-Github
 /usr/bin/git add /root/qinGitHub/Hello-Github
+echo "2"
 /usr/bin/git commit -m 'autoUpload'
+echo "3"
 /usr/bin/git push
+echo "4"
